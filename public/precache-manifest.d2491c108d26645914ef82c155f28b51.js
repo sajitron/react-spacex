fe0a967066a21d7ed8a127b86ad2ed730708bba3
sajicode <sajioloye@gmail.com>
2018-12-16 05:53:08 +0100

    build react app into public folder

---
+++ public/precache-manifest.d2491c108d26645914ef82c155f28b51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f1089c39fb6f71a6c55b",
+    "url": "/static/js/main.f1089c39.chunk.js"
+  },
+  {
+    "revision": "a6812aefad2831ee0241",
+    "url": "/static/js/1.a6812aef.chunk.js"
+  },
+  {
+    "revision": "f1089c39fb6f71a6c55b",
+    "url": "/static/css/main.0716e58c.chunk.css"
+  },
+  {
+    "revision": "7096591a9ded36f100b2758319fe44d9",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const cors = require('cors');
 const schema = require('./schema');
+const path = require('path');
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true
-}));
+// Allow cross-origin
+app.use(cors());
 
-const PORT = process.env.PORT || 5000
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema,
+		graphiql: true
+	})
+);
+
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server started on PORT ${PORT}`);
+	console.log(`Server started on PORT ${PORT}`);
 });
